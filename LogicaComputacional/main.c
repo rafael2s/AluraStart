00654f407d1d7db395be98af639e50255fd9705a
rafael2s <rafael_popo@hotmail.com>
2020-12-10 19:04:18 -0300

    Aula 02

--- LogicaComputacional/main.c
+++ LogicaComputacional/main.c
@@ -13,13 +13,31 @@ int main(){
 
     // Jogo
     int numeroSecreto = 42;
-    printf("O número secreto é %d.\n", numeroSecreto);
+    //printf("O número secreto é %d.\n", numeroSecreto);
 
     int chute;
     printf("Qual é o seu chute? ");
     scanf("%d", &chute);
 
-    printf("O seu chute é %d, enquanto o número secreto é %d.\n", chute, numeroSecreto);
+    printf("O seu chute é %d.\n", chute);
+
+    int acertou = (chute == numeroSecreto);
+
+    if (acertou) {
+        printf("Parabéns! Você acertou!\n");
+        printf("Jogue novamente!\n");
+    } else {
+        printf("Que pena, você errou dessa vez!\n");
+        printf("Não desanime, tente novamente!\n");
+
+        int maior = (numeroSecreto > chute);
+        if(maior) {
+            printf("O número secreto é maior que %d.\n", chute);
+        } else {
+            printf("O número secreto é menor que %d.\n", chute);
+        }
+    }
+
 
     return 0;
 }

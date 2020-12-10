46b7eb0db356c75aa630b1ee0510b59d05a14d4f
rafael2s <rafael_popo@hotmail.com>
2020-12-10 18:03:08 -0300

    Aula 01

--- LogicaComputacional/main.c
+++ LogicaComputacional/main.c
@@ -6,20 +6,20 @@ int main(){
 
     setlocale(LC_ALL, "Portuguese");
 
+    // Imprime mensagem de boas vindas
     printf("******************************************\n");
     printf("* Bem-vindo ao nosso jogo de adivinhação *\n");
     printf("******************************************\n");
 
-    int numeroSecreto;
-    numeroSecreto = 42;
-
+    // Jogo
+    int numeroSecreto = 42;
     printf("O número secreto é %d.\n", numeroSecreto);
 
     int chute;
-    printf("Qual é o seu chute?");
-    scanf("%d", chute);
+    printf("Qual é o seu chute? ");
+    scanf("%d", &chute);
 
-    printf("O seu chute é %d.\n", chute);
+    printf("O seu chute é %d, enquanto o número secreto é %d.\n", chute, numeroSecreto);
 
     return 0;
 }
